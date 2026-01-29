c1d2a659ea7179d0ca5cf92297f6c6152d402ce9
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-01-29 16:15:03 +0530

    Update 04.sequence.sv

--- 02.D_flipflop_verification.sv/04.sequence.sv
+++ 02.D_flipflop_verification.sv/04.sequence.sv
@@ -1,13 +1,28 @@
-class dff_seq_item extends uvm_sequence_item;
+class dff_sequence extends uvm_sequence#(dff_seq_item);
   
-  `uvm_object_utils(dff_seq_item)
+  `uvm_object_utils(dff_sequence)
   
-  bit clk;
-  bit rst;
-  rand bit d;
-  bit q;
-  
-  function new(name="dff_seq_item");
+  function new(name="dff_sequence");
     super.new(name);
   endfunction
+  
+  task body();
+    
+    dff_seq_item req;
+    
+    `uvm_info("SEQ","Sequence starts",UVM_LOW)
+    
+    repeat(10) begin
+    
+    req= dff_seq_item::type_id::create("req");
+    start_item(req);
+    if(!req.randomize()) `uvm_error("SEQ", "Randomize failed");
+      finish_item(req);
+    end
+    
+  endtask
 endclass
+    
+    
+  
+  
