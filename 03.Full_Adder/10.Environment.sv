7cb50b975b16144106af65cb4001438825e446f8
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-01-30 18:57:42 +0530

    Create 10.Environment.sv

---
+++ 03.Full_Adder/10.Environment.sv
@@ -0,0 +1,36 @@
+
+
+
+class FA_env extends uvm_env;
+  
+  `uvm_component_utils(FA_env);
+  
+  FA_agent agent;
+  FA_scoreboard sb;
+  
+  function new(string name="environment", uvm_component parent);
+    super.new(name, parent);
+    
+    `uvm_info("ENV", "Environment Created", UVM_LOW)
+    
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    
+    super.build_phase(phase);
+    
+    agent= FA_agent:: type_id ::create("agent",this);
+    
+    sb=FA_scoreboard::type_id:: create("sb",this);
+    
+  endfunction
+  
+  function void  connect_phase (uvm_phase phase);
+    
+    super.connect_phase(phase);
+    
+    agent.montr.mon_ap.connect(sb.sb_imp);
+    
+  endfunction
+  
+endclass
