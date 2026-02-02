66465c543f365ecd35d369d85679afc1b6edfa8a
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-02-02 19:13:55 +0530

    Create 09.Agent.sv

---
+++ 04.Memory_verification/09.Agent.sv
@@ -0,0 +1,30 @@
+
+
+class mem_agent extends uvm_agent;
+  
+  `uvm_component_utils(mem_agent)
+  
+  mem_driver drvr;
+  mem_monitor montr;
+  mem_sequencer seqncr;
+  
+  function new(string name="Agent", uvm_component parent);
+    super.new(name, parent);
+    
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    
+    seqncr=mem_sequencer:: type_id :: create("seqncr", this);    
+    drvr=mem_driver::type_id::create("drvr",this);
+    montr=mem_monitor:: type_id :: create("montr",this);
+    
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    drvr.seq_item_port.connect(seqncr.seq_item_export);
+  endfunction
+  
+endclass
