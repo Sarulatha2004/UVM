6c815a2ec29ed8f6c408a6e95554a8a8d1a87b95
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-02-02 19:15:01 +0530

    Create 11.Test.sv

---
+++ 04.Memory_verification/11.Test.sv
@@ -0,0 +1,36 @@
+class mem_test extends uvm_test;
+  
+  `uvm_component_utils(mem_test)
+  
+  mem_env env;
+  mem_sequence seq;
+
+  function new(string name="test", uvm_component parent);
+    super.new(name, parent);
+    
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    
+    super.build_phase(phase);
+    
+    env=mem_env:: type_id::create("env",this);
+    seq=mem_sequence::type_id::create("seq");
+
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    
+    super.run_phase(phase);
+    
+    phase.raise_objection(this);
+    
+    
+    seq.start(env.agent.seqncr);
+    
+    #200;
+    
+    phase.drop_objection(this);
+    
+  endtask
+endclass
