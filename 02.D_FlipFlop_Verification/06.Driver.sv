9ebdde22929a29e218d4f68715f3588379869e33
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-02-02 17:28:50 +0530

    Create 06.Driver.sv

---
+++ 02.D_FlipFlop_Verification/06.Driver.sv
@@ -0,0 +1,45 @@
+class dff_driver extends uvm_driver#(dff_seq_item);
+  
+  `uvm_component_utils(dff_driver)
+  
+  
+  virtual dff_if vif;
+  dff_seq_item req;
+  
+  function new(name="dff_driver",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(!uvm_config_db  #(virtual dff_if)::get(this,"","vif",vif))
+      `uvm_fatal("DRIVER","Virtual interface not found");
+  endfunction
+      
+  
+  
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+  forever begin
+    req=dff_seq_item::type_id::create("req");
+
+    seq_item_port.get_next_item(req);
+
+    @(negedge vif.clk);     
+
+    vif.rst <= req.rst;
+    vif.d   <= req.d;
+
+    @(posedge vif.clk);  
+    #10;
+
+    seq_item_port.item_done();
+
+  end
+
+endtask
+
+endclass
+    
+        
