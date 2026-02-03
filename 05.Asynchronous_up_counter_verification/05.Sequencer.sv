ff4a7742a77f0aca2a5f9dd87ffd6a05d37478db
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-02-03 15:21:28 +0530

    Create 05.Sequencer.sv

---
+++ 05.Asynchronous_up_counter_verification/05.Sequencer.sv
@@ -0,0 +1,8 @@
+class cntr_sequencer extends uvm_sequencer#(cntr_seq_item);
+  
+  `uvm_component_utils(cntr_sequencer)
+  
+  function new(string name="cntr_sequencer",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+endclass
